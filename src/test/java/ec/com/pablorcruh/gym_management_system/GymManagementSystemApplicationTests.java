a8de9a53b5a9ebe800c500639fa6e6a0be27adfb
pablorcruh <pablorcruh@gmail.com>
2024-04-23 15:14:47 -0500

    initial commit

---
+++ src/test/java/ec/com/pablorcruh/gym_management_system/GymManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package ec.com.pablorcruh.gym_management_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
