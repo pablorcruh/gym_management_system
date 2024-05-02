8dc46626c27d87537125b1c8a9860f37eec8b098
pablorcruh <pablorcruh@gmail.com>
2024-05-01 21:58:47 -0500

    add main company relationship to schema

---
+++ src/main/java/ec/com/pablorcruh/gym_management_system/domain/models/MainCompany.java
@@ -0,0 +1,16 @@
+package ec.com.pablorcruh.gym_management_system.domain.models;
+
+import java.util.Set;
+
+public class MainCompany {
+    private String name;
+
+    private String ruc;
+
+    private String email;
+
+    private String phoneNumber;
+
+    private Set<Campus> campuses;
+
+}
